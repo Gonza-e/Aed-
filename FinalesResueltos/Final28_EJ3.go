d2aea80484899386d48328afbd0efd744b3c6839
Gonza-e <Gonzaloescobar0904@gmail.com>
2025-05-30 01:19:42 -0300

    Ejercicio agregado a finales resueltos

---
+++ FinalesResueltos/Final28_EJ3.go
@@ -0,0 +1,23 @@
+Accion final28 es 
+
+	Arbol = registro 
+		dato: entero 
+		izq: puntero a nodo 
+		der: puntero a nodo 
+	freg 
+	prim: puntero a nodo 
+
+	Funcion buscar(prim: puntero a nodo, dato: entero):entero 
+		si prim.dato = dato entonces 
+			buscar:= dato 
+		sino 
+			si *prim.dato < dato entonces 
+				buscar:= buscar(*prim.der,dato)
+			sino 
+				si *prim.dato > dato 
+					buscar:= buscar(*prim.izq,dato)
+				fsi 
+			fsi 
+		fsi 
+	FFuncion 
+FAccion
