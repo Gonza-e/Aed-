06872a8f51d3ce30708c5e0558923f75e5fd73e2
Gonza-e <Gonzaloescobar0904@gmail.com>
2025-08-08 16:39:49 -0300

    Agregado a Python

---
+++ Python/CasiPerfecto.py
@@ -0,0 +1,20 @@
+def numPerfecto(num1,suma,num2: int) -> bool:
+    if (num2 == 0):
+        if (suma == num1 - 1): 
+            return True 
+        else: 
+            return False 
+    else: 
+        if ((num1 % num2) == 0): 
+            return numPerfecto(num1, suma + num2, num2 - 1)
+        else: 
+            return numPerfecto(num1, suma, num2 - 1)
+        
+
+print("Ingrese un numero")
+numero = int(input())
+
+if numPerfecto(numero, 0,numero - 1): 
+    print("El numero es casi perfecto")
+else: 
+    print("El numero no es casi perfecto")
