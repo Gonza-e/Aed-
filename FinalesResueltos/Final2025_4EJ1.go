81a6c2a1bb664c15f63672c6f3310ae025ecfcfe
Gonza-e <Gonzaloescobar0904@gmail.com>
2025-08-14 14:12:25 -0300

    Ejercicio corregido

--- FinalesResueltos/Final2025_4EJ1.go
+++ FinalesResueltos/Final2025_4EJ1.go
@@ -1,24 +1,9 @@
-/*1. Se tiene almacenado los datos de pacientes en una secuencia de caracteres
-Historia clinica(4 caracteres)Nombre-TipoDeCobertura(de 10 a 29)TipoDeEspecialidad(0 a 9)Dia('L','M','V')
-
-diseñar un algoritmo que muestre un menu con la siguientes acciones :
-    - separar los datos en HistoriaClinica y Tipo de Especialidad  (no me acuerdo si eran exactamente esos dato, pero si pedia 2).
-    - buscar a un paciente y si se encuentra, informar el Historia Clinica, tipo de Cobertura y Dia.
-    - informar: 
-        - total por tipo de especialidad y tipo de cobertura
-        - el tipo de especialidad que mas se solicitó
-        - el tipo de cobertura que menos se registró.
-    Se puede usar la funcion CONCATENAR ----> la funcion concantena caracteres, los parametros son la variable donde guardar
-	y el caracter a concatenar (ejemplo:= CONCATENAR (ejemplo,v)). Se usa dentro de un ciclo.
-
-Utilizar la estructura que crea conveniente*/
-
 Accion ejercicio1 es 
  Ambiente 
     sec: secuencia de caracteres
     v: caracter
     nombre, hist, d: AN 
-    t_cobertura, t_especialidad, pos_may, e_may, pos_men, c_men: entero 
+    t_cobertura, t_especialidad, pos_may, e_may, pos_men, c_men, opcion, salir: entero 
 
     nodo = registro 
         nombre: AN(30)
@@ -30,8 +15,18 @@ Accion ejercicio1 es
     FReg 
     p,q,ant,prim: puntero a nodo 
     
-    A: arreglo[1...19] de enteros 
-    B: arreglo[1...10] de enteros 
+    nodo2 = registro 
+        nombre: AN(30)
+        hist_c: AN(4)
+        tipo_especialidad: N(2)
+        tipo_cobertura: N(1)
+        dia: AN(1)
+        prox: puntero a nodo2 
+    FReg 
+    p2,q2,ant2,prim2: puntero a nodo2
+
+    A: arreglo[10...29] de enteros 
+    B: arreglo[0...9] de enteros 
 
     funcion conv_entero(v:caracter):entero 
         ="0": conv_entero:= 0
@@ -45,22 +40,99 @@ Accion ejercicio1 es
         ="8": conv_entero:= 8
         ="9": conv_entero:= 9
     FFuncion
+    
+    procedimiento cargarLista() es 
+        Mientras NFDS(sec) hacer 
+            para i:= 1 hasta 4 hacer 
+                Concatenar(hist,v)
+                AVZ(sec,v)
+            FPara 
+            Mientras v <> "-" hacer 
+                Concatenar(nombre,v)
+                AVZ(sec,v)
+            FM 
+            AVZ(sec,v)
 
-    procedimiento carga_ordenada() es 
-        p:= prim; ant:= nil 
-        Mientras (p<>nil) y ((*q.hist_c > *p.hist_c) y (*q.t_especialidad > *p.t_especialidad)) hacer 
-            ant:= p 
+            para i:= 1 hasta 2 hacer 
+                t_cobertura:= (t_cobertura * 10) + conv_entero(v)
+                AVZ(sec,v)
+            FPara
+
+            t_especialidad:= conv_entero(v)
+            AVZ(sec,v)
+            d:= v 
+
+            A[t_cobertura]:= A[t_cobertura] + 1 
+            B[t_especialidad]:= B[t_especialidad] + 1 
+
+            Nuevo(q); *q.hist_c:= hist; *q.nombre:= nombre; *q.tipo_cobertura:= t_cobertura
+            *q.tipo_especialidad:= t_especialidad; *q.dia:= d 
+
+            *q.prox:= prim 
+            prim:= q 
+        FM     
+        Cerrar(sec)
+    FProcedimiento
+
+    procedimiento cargaOrdenada() es 
+        p2:= prim2; ant2:= nil 
+        Mientras (p2<>nil) y ((*q.dia < *p2.dia) o ((*q.dia = *p2.dia) y (*q.hist_c < *p2.hist_c))) hacer 
+            ant2:= p2 
+            p2:= *p2.prox 
+        FM 
+
+        si p2 = prim2 entonces 
+            *q.prox:= p2 
+            prim2:= q2 
+        sino 
+            *ant2.prox:= q2 
+            *q2.prox:= p2 
+        fsi 
+    FProcedimiento 
+
+    procedimiento ordenarLista() es 
+        Nuevo(q); *q.nombre:= *p.nombre; *q.hist_c:= *p.hist_c; *q.tipo_cobertura:= *p.tipo_cobertura
+        *q.tipo_especialidad:= *p.tipo_especialidad; *q.dia:= *p.dia
+
+        si prim2 = nil entonces 
+            *q.prox:= prim2 
+            prim2:= q 
+        sino 
+            cargaOrdenada()
+        fsi        
+    FProcedimiento
+
+    procedimiento busqueda() es 
+        Esc("Ingrese nombre del paciente: "); Leer(nom_paciente)
+
+        Mientras (p<>nil) y *p.nombre <> nom_paciente hacer 
             p:= *p.prox 
         FM 
 
-        si p = prim entonces 
-            *q.prox:= p 
-            prim:= q 
+        si *p.nombre = nom_paciente entonces 
+            Esc("Especialidad: ",*p.tipo_especialidad," Dia: ",*p.dia)
         sino 
-            *ant.prox:= q 
-            *q.prox:= p 
+            Esc("No se encontro al paciente")
         fsi 
-    FProcedimiento 
+    FProcedimiento
+
+    procedimiento informar() es 
+        para i:= 1 hasta 10 hacer 
+            Esc("Pacientes atendidos en la especialidad ",i,": ",A[i])
+            si e_may < A[i] entonces 
+                e_may:= A[i]
+                pos_may:= i 
+            fsi 
+        FPara 
+
+        para i:= 1 hasta 19 hacer 
+            Esc("Pacientes atendidos con la cobertura ",i,": ",B[i])
+            si c_men > B[i] entonces 
+                c_men:= B[i]
+                pos_men:= i 
+            fsi 
+        FPara 
+    FProcedimiento
 
     Proceso 
         ARR(sec); AVZ(sec)
@@ -74,72 +146,20 @@ Accion ejercicio1 es
         nombre:= " "; t_cobertura:= 0; hist:= " "; t_especialidad:= 0; d:= " "
         pos_may:= 0; pos_men:= 0; e_may:= LV; c_men:= HV 
 
-        Mientras NFDS(sec) hacer 
-            Para i:= 1 a 4 hacer 
-                concatenar(hist,v)
-                AVZ(sec,v)
-            FPara
+        cargarLista()
+        repetir 
+            Esc("Ingrese la operacion que quiere: (1: ordenar, 2: buscar paciente, 3: informar)")
+            Leer(opcion)
 
-            Mientras v <> "-" hacer 
-                concatenar(nombre,v)
-                AVZ(sec,v)
-            FM
-            AVZ(sec,v)
-            
-            Para i:= 1 a 2 hacer 
-                t_cobertura:= (t_cobertura * 10) + conv_entero(v)
-                AVZ(sec,v)
-            FPara 
+            segun opcion hacer 
+                1: ordenarLista()
+                2: busqueda()
+                3: informar()
+            FSegun
 
-            t_especialidad:= conv_entero(v) 
-            AVZ(sec,v)
-            d:= v 
-            AVZ(sec,v)
-
-            A[t_cobertura]:= A[t_cobertura] + 1 
-            B[t_especialidad]:= B[t_especialidad] + 1  
-
-            nuevo(q); *q.nombre:= nombre; *q.hist_c:= hist; *q.tipo_cobertura:= t_cobertura; *q.dia:= d 
-            si prim = nil entonces
-                *q.prox:= prim 
-                prim:= q 
-            sino 
-                carga_ordenada()
-            fsi 
-
-            t_cobertura:= 0
-        FM 
-
-        Esc("Ingrese el nombre del paciente")
-        Leer(nom_paciente)
-        Cerrar(sec)
-        
-        p:= prim
-        Mientras p <> nil y (nom_paciente <> *p.nombre) hacer
-            p:= *p.prox 
-        FM 
-        si p = nil entonces 
-            Esc("El paciente no se encontro")
-        sino 
-            Esc("Historia clinica: ",*p.hist," Tipo de cobertura: ",*p.tipo_cobertura," Dia: ",*p.dia)
-        fsi 
-
-        Para i:= 1 a 19 hacer 
-            Esc("Especialidad: ",i," Cantidad: ",A[i])
-            si e_may < A[i] entonces 
-                e_may:= A[i]
-                pos_may:= i
-            fsi 
-        FPara 
-        Para i:= 1 a 10 hacer 
-            Esc("Cobertura: ",i," Cantidad: ",B[i])
-            si c_men > B[i] entonces 
-                c_men:= B[i]
-                pos_men:= i
-            fsi 
-        FPara 
-
-        Esc("La especialidad mayor fue: ",pos_may); Esc("La cobertura menor fue: ",pos_men)
+            Esc("Desea seguir? (1: no, 2: si)")
+            Leer(salir)
+        hasta que (salir = 1)
     FProceso 
 FAccion 
 
