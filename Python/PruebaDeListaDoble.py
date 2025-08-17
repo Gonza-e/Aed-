67eae134273561819a1d671fd0593fe0d6834e5c
Gonza-e <Gonzaloescobar0904@gmail.com>
2025-08-16 22:21:54 -0300

    Funcion recursiva para saber si una palabra es capicua

---
+++ Python/PruebaDeListaDoble.py
@@ -0,0 +1,58 @@
+class Nodo:
+    def __init__(self, letra):
+        self.letra = letra
+        self.prox = None
+        self.ant = None
+
+class ListaDoble:
+    def __init__(self):
+        self.prim = None
+        self.ult = None
+
+    # Función recursiva para verificar capicúa 
+    def capicua(self, proximo, anterior, bandera=True):
+        if proximo == anterior and bandera:  # caso base
+            return True
+        else:
+            if proximo and anterior and proximo.letra == anterior.letra:
+                return self.capicua(proximo.prox, anterior.ant, True)
+            else:
+                return False
+
+    # Procedimiento de inserción al final
+    def insertar(self, letra):
+        nuevo = Nodo(letra)
+
+        if self.prim is None:  # lista vacía
+            self.prim = nuevo
+            self.ult = nuevo
+        else:
+            nuevo.ant = self.ult
+            self.ult.prox = nuevo
+            self.ult = nuevo
+
+    # Método para mostrar lista (opcional, ayuda en prueba) 
+    def mostrar(self):
+        p = self.prim
+        while p:
+            print(p.letra, end=" <-> ")
+            p = p.prox
+        print("Nil")
+
+# --- Programa Principal ---
+lista = ListaDoble()
+print("Presione 0 para terminar")
+letra = ' '
+
+while letra != "0":
+    letra = input("Ingrese una letra: ")
+    if letra != "0":
+        lista.insertar(letra)
+
+
+print("\nLista cargada:")
+lista.mostrar()
+
+# Verificar si es capicúa
+es_capicua = lista.capicua(lista.prim, lista.ult)
+print("\n¿La lista es capicúa?:", "Sí" if es_capicua else "No")
